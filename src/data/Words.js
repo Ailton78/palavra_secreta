13e8b15e78149a81252e9bd8d86e8dbba9dc0fb9
Ailton Bulhões Sampaio <sd.ailtonsampaio@gmail.com>
2022-08-27 14:45:38 -0300

    first commit

---
+++ src/data/Words.js
@@ -0,0 +1,14 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: [
+    "Linguagem",
+    "Framework",
+    "JavaScript",
+    "React",
+    "Desenvolvedor",
+  ],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
